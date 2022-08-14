97acff40bedcb7bd599e238ebe5435d80ad44180
Haobin Chen <etyyuiope@qq.com>
2022-08-13 22:24:18 -0400

    Added regs zeroization

--- src/clear_stack_on_return.rs
+++ src/clear_stack_on_return.rs
@@ -1,5 +1,37 @@
+use core::arch::asm;
 use crate::fnoption::FnOption;
 use crate::hide::{hide_mem, hide_ptr};
+
+/// Once arguments are classified, the registers get assigned for passing as follows:
+///
+/// 1. If the class is MEMORY, pass the argument on the stack.
+/// 2. If the class is INTEGER, the next available register of the sequence
+///    %rdi, %rsi, %rdx, %rcx, %r8 and %r9 is used.
+#[inline]
+#[cfg(target_arch = "x86_64")]
+#[cfg(feature = "sgx")]
+fn clean_registers() {
+  unsafe {
+    // Is there any need to clear RBP / RSP?
+    // asm!("xor rbp, rbp /* {0} */", out(reg) _);
+    // asm!("xor rsp, rsp /* {0} */", out(reg) _);
+
+    asm!("xor rax, rax /* {0} */", out(reg) _);
+    asm!("xor rbx, rbx /* {0} */", out(reg) _);
+    asm!("xor rcx, rcx /* {0} */", out(reg) _);
+    asm!("xor rdx, rdx /* {0} */", out(reg) _);
+    asm!("xor rdi, rdi /* {0} */", out(reg) _);
+    asm!("xor rsi, rsi /* {0} */", out(reg) _);
+    asm!("xor r8, r8 /* {0} */", out(reg) _);
+    asm!("xor r9, r9 /* {0} */", out(reg) _);
+    asm!("xor r10, r10 /* {0} */", out(reg) _);
+    asm!("xor r11, r11 /* {0} */", out(reg) _);
+    asm!("xor r12, r12 /* {0} */", out(reg) _);
+    asm!("xor r13, r13 /* {0} */", out(reg) _);
+    asm!("xor r14, r14 /* {0} */", out(reg) _);
+    asm!("xor r15, r15 /* {0} */", out(reg) _);
+  }
+}
 
 /// Calls a closure and overwrites its stack on return.
 ///
@@ -21,13 +53,17 @@ use crate::hide::{hide_mem, hide_ptr};
 /// let result = clear_stack_on_return(1, || encrypt(input));
 /// ```
 #[inline]
-pub fn clear_stack_on_return<F, R>(pages: usize, mut f: F) -> R
-where
-    F: FnMut() -> R,
+pub fn clear_stack_on_return<F, R>(pages: usize, mut f: F, clean_regs: bool) -> R
+  where
+      F: FnMut() -> R,
 {
-    let _clear = ClearStackOnDrop { pages };
-    // Do not inline f to make sure clear_stack uses the same stack space.
-    hide_ptr::<&mut dyn FnMut() -> R>(&mut f)()
+  let _clear = ClearStackOnDrop { pages, clean_regs };
+  // Do not inline f to make sure clear_stack uses the same stack space.
+  let hidden_func = hide_ptr::<&mut dyn FnMut() -> R>(&mut f);
+
+  // TODO: Calculate the estimated size of the stack frame.
+
+  hidden_func()
 }
 
 /// Calls a closure and overwrites its stack on return.
@@ -44,35 +80,46 @@ where
 /// let result = clear_stack_on_return_fnonce(1, || encrypt(input));
 /// ```
 #[inline]
-pub fn clear_stack_on_return_fnonce<F, R>(pages: usize, f: F) -> R
-where
-    F: FnOnce() -> R,
+pub fn clear_stack_on_return_fnonce<F, R>(pages: usize, f: F, clean_regs: bool) -> R
+  where
+      F: FnOnce() -> R,
 {
-    let mut f = FnOption::new(f);
-    clear_stack_on_return(pages, || f.call_mut()).unwrap()
+  let mut f = FnOption::new(f);
+  clear_stack_on_return(pages, || f.call_mut(), clean_regs).unwrap()
 }
 
 struct ClearStackOnDrop {
-    pages: usize,
+  pages: usize,
+  clean_regs: bool,
 }
 
 impl Drop for ClearStackOnDrop {
-    #[inline]
-    fn drop(&mut self) {
-        // Do not inline clear_stack.
-        hide_ptr::<fn(usize)>(clear_stack)(self.pages);
-    }
+  #[inline]
+  fn drop(&mut self) {
+    // Do not inline clear_stack.
+    hide_ptr::<fn(usize, bool)>(clear_stack)(self.pages, self.clean_regs);
+  }
 }
 
 /// Overwrites a few pages of stack.
 ///
 /// This function will overwrite `pages` 4096-byte blocks of the stack
 /// with zeros.
-pub fn clear_stack(pages: usize) {
-    if pages > 0 {
-        let mut buf = [0u8; 4096];
-        hide_mem(&mut buf); // prevent moving after recursive call
-        clear_stack(pages - 1);
-        hide_mem(&mut buf); // prevent reuse of stack space for call
-    }
+///
+/// ```asm
+/// call    __rust_probestack
+/// sub     rsp, rax
+/// test    rdi, rdi
+/// jz      short loc_188C8
+/// ```
+pub fn clear_stack(pages: usize, clean_regs: bool) {
+  // When the cleaner is about to finish, we write garbage data to all the registers.
+  if pages == 0 && clean_regs {
+    clean_registers();
+  } else {
+    let mut buf = [0u8; 4096];
+    hide_mem(&mut buf); // prevent moving after recursive call
+    clear_stack(pages - 1, clean_regs);
+    hide_mem(&mut buf); // prevent reuse of stack space for call
+  }
 }
