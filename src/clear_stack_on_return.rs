2dc5ea749b11030a2aefb4cd2c110e4eb98b5ab4
Haobin Chen <etyyuiope@qq.com>
2022-08-13 23:07:59 -0400

    Refactored interface

--- src/clear_stack_on_return.rs
+++ src/clear_stack_on_return.rs
@@ -11,10 +11,6 @@ use core::arch::asm;
 #[cfg(target_arch = "x86_64")]
 fn clean_registers() {
     unsafe {
-        // Is there any need to clear RBP / RSP?
-        // asm!("xor rbp, rbp /* {0} */", out(reg) _);
-        // asm!("xor rsp, rsp /* {0} */", out(reg) _);
-
         asm!("xor rax, rax /* {0} */", out(reg) _);
         asm!("xor rbx, rbx /* {0} */", out(reg) _);
         asm!("xor rcx, rcx /* {0} */", out(reg) _);
@@ -30,6 +26,30 @@ fn clean_registers() {
         asm!("xor r14, r14 /* {0} */", out(reg) _);
         asm!("xor r15, r15 /* {0} */", out(reg) _);
     }
+}
+
+/// Calls a closure and overwrites its stack and registers on return.
+/// 
+/// For simplicity, we consider the closure as a FnOnce for most critical
+/// procedures, and do not provide interface for FnMut.
+/// 
+/// # Example
+///
+/// ```
+/// # use clear_on_drop::clear_stack_and_regs_on_return;
+/// # fn encrypt(input: &[u8]) -> Vec<u8> { input.to_owned() }
+/// let input = b"abc";
+/// let result = clear_stack_and_regs_on_return(1, || encrypt(input));
+/// ```
+#[inline]
+pub fn clear_stack_and_regs_on_return<F, R>(pages: usize, f: F) -> R
+where
+    F: FnOnce() -> R,
+{
+    let _clear = ClearStackOnDrop { pages, true };
+    let hidden_func = hide_ptr::<&mut dyn FnMut() -> R>(&mut f);
+
+    hidden_func()
 }
 
 /// Calls a closure and overwrites its stack on return.
@@ -52,16 +72,13 @@ fn clean_registers() {
 /// let result = clear_stack_on_return(1, || encrypt(input));
 /// ```
 #[inline]
-pub fn clear_stack_on_return<F, R>(pages: usize, mut f: F, clean_regs: bool) -> R
+pub fn clear_stack_on_return<F, R>(pages: usize, mut f: F) -> R
 where
     F: FnMut() -> R,
 {
-    let _clear = ClearStackOnDrop { pages, clean_regs };
+    let _clear = ClearStackOnDrop { pages, false };
     // Do not inline f to make sure clear_stack uses the same stack space.
     let hidden_func = hide_ptr::<&mut dyn FnMut() -> R>(&mut f);
-
-    // TODO: Calculate the estimated size of the stack frame.
-
     hidden_func()
 }
 
@@ -79,12 +96,12 @@ where
 /// let result = clear_stack_on_return_fnonce(1, || encrypt(input));
 /// ```
 #[inline]
-pub fn clear_stack_on_return_fnonce<F, R>(pages: usize, f: F, clean_regs: bool) -> R
+pub fn clear_stack_on_return_fnonce<F, R>(pages: usize, f: F) -> R
 where
     F: FnOnce() -> R,
 {
     let mut f = FnOption::new(f);
-    clear_stack_on_return(pages, || f.call_mut(), clean_regs).unwrap()
+    clear_stack_on_return(pages, || f.call_mut()).unwrap()
 }
 
 struct ClearStackOnDrop {
